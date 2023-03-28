ba6f4f9a4f1606c73498c9b052e9979f58224073
SAKTHI M <msakthi150@gmail.com>
2023-03-28 20:16:38 +0530

    commit

--- a_25_jagged_array.java
+++ a_25_jagged_array.java
@@ -1,5 +1,29 @@
 public class a_25_jagged_array {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        int [][] arr=new int [][]
+                {
+                {1,2,3,4},
+                {1,2,3,4,5},
+                {1,2,3}
 
+                };
+        for(int i=0;i<arr.length;i++)
+        {
+            for(int j=0;j<arr[i].length;j++)
+            {
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.print('\n');
+        }
+        System.out.print("Using for each loop");
+        for(int k[]:arr)
+        {
+            for(int l:k)
+            {
+                System.out.print(l+" ");
+            }
+            System.out.print('\n');
+        }
     }
 }
