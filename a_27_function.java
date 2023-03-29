4394e4878ef547e5d8126456084648c6669a3887
SAKTHI M <msakthi150@gmail.com>
2023-03-29 06:09:40 +0530

    commit

--- a_27_function.java
+++ a_27_function.java
@@ -1,11 +1,10 @@
 public class a_27_function {
-    public int add(int a,int b)
+    public static int add(int a,int b)
     {
         return a+b;
     }
     public static void main(String[] args) {
         int a=12,b=13;
-        a_27_function obj= new a_27_function();
-        System.out.println(obj.add(a,b));
+        System.out.println(add(a,b));
     }
 }
